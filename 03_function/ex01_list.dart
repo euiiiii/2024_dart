f6f0dab10270f6376a40040972ec5ab2baf23699
leeeuijin <kouy96@naver.com>
2024-12-20 13:09:15 +0900

    241220 13:09 dart function-map까지지

---
+++ 03_function/ex01_list.dart
@@ -0,0 +1,46 @@
+void main() {
+  // 리스트는 중복이 가능하다.
+  // 리스트를 다른 자료형으로 변경
+  List<String> cool = ['이재훈','유리','김성수','유리'];
+  print(cool);
+  print(cool.asMap());
+  print(cool.toSet()); // 중복 안 됨
+  print('-'*50);
+
+  Map coolMap = cool.asMap();
+  print(coolMap);
+  print(coolMap.keys); // Iterable 형태 ()
+  print(coolMap.values); // Iterable 형태는 사용하지 않고 다른 타입으로 변경한다.
+  print('-'*50);
+
+
+  print(coolMap.values.toList());
+  print(coolMap.values.toSet());
+  print('-'*50);
+
+  // 리스트를 Set으로 만드는 방법
+  Set coolSet = Set.from(cool);
+  print(coolSet);
+  print('-'*50);
+
+  var n_cool = cool.map((x) {
+    return '쿨 ${x}';
+  });
+  print(n_cool); // Iterable
+  print(n_cool.toList());
+  print('-'*50);
+
+  var a_cool = cool.map((x) => '쿨2 ${x}');
+  print(a_cool); // Iterable
+  print(a_cool.toList());
+  print('-'*50);
+
+  String number = '13579';
+  print(number);
+  final res = number.split('');
+  print(res); // 리스트로 나온다.
+
+  // 1.jpg, 3.jpg, 5.jpg, 7.jpg, 9.jpg
+  final res2 = number.split('').map((k) => '${k}.jpg').toList();
+  print(res2);
+}
